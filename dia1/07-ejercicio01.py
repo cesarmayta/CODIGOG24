2b66bbe3faab4f7e0db86031a61426d7bd1ac12e
servilex <schabaneix@servilex.pe>
2024-01-22 22:43:13 -0500

    dia1 ejercicio 01

---
+++ dia1/07-ejercicio01.py
@@ -0,0 +1,13 @@
+"""
+crear un programa que ingrese un numero y cree un cuadrado con * en base al numero
+ingresado
+
+ejemplo : num = 5
+resultado:
+
+* * * * * 
+*       *
+*       *
+*       *
+* * * * *
+"""
