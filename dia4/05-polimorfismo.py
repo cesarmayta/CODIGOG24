5117a982b5005c211b4c57b3f356abfd54b0410a
servilex <schabaneix@servilex.pe>
2024-01-25 20:42:13 -0500

    polimorfismo

--- dia4/05-polimorfismo.py
+++ dia4/05-polimorfismo.py
@@ -5,7 +5,7 @@ class Persona:
         self.email = ema
         
     def mostrar(self):
-        print("="*30)
+        print("*"*10 + " DATOS PERSONALES" + "*"*10)
         print("NOMBRE : " + self.nombre)
         print("EMAIL : " + self.email)
     
@@ -15,11 +15,22 @@ class Alumno(Persona):
         super().__init__(nom,ema)
         self.nota = nota
         
+    def mostrar(self):
+        super().mostrar()
+        print(("NOTA : " + str(self.nota)))
+        
 class Profesor(Persona):
     
     def __init__(self,nom,ema,esp):
         super().__init__(nom,ema)
         self.especialidad = esp
+        
+    def mostrar(self):
+        super().mostrar()
+        print("ESPECIALIDAD :" + self.especialidad)
+        
+class Empleado(Persona):
+    pass
         
 
 alumno1 = Alumno('CESAR','cesar@gmail.com',20)
@@ -27,3 +38,6 @@ alumno1.mostrar()
 
 profesor1 = Profesor('GUILLERMO','guille@gmail.com','REACT')
 profesor1.mostrar()
+
+empleado = Empleado('JULIO','julio@gmail.com')
+empleado.mostrar()
