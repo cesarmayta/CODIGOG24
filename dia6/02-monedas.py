52f98132032d0dd54f06c6e4015db90db3ad336b
servilex <schabaneix@servilex.pe>
2024-01-27 11:42:19 -0500

    dia 6 funciones

--- dia6/02-monedas.py
+++ dia6/02-monedas.py
@@ -3,6 +3,31 @@ CREAR UN PROGRMAA QUE CONVIERTA MONEDAS DE SOLES A DOLARES
 """
 #ENTRADA
 TIPO_CAMBIO = 3.786
+opcion_1 = {
+    'moneda_origen':'soles',
+    'moneda_destino':'dolares',
+    'operador':'/'
+}
+opcion_2 = {
+    'moneda_origen':'dolares',
+    'moneda_destino':'soles',
+    'operador':'*'
+}
+#FUNCIONES
+def convertir_monto(convertir_opcion):
+    #ENTRADA
+    moneda_origen = convertir_opcion.get('moneda_origen')
+    moneda_destino = convertir_opcion.get('moneda_destino')
+    operador = convertir_opcion.get('operador')
+    #PROCESO
+    monto_origen = float(input(f'Ingrese monto en {moneda_origen} :'))
+    if operador == "/":
+        monto_destino = monto_origen / TIPO_CAMBIO
+    elif operador == "*":
+        monto_destino = monto_origen * TIPO_CAMBIO
+    #SALIDA
+    print(f'El monto en {moneda_destino} es {monto_destino}')
+
 print("""
       ==============================================
                 CONVERTIDOR DE MONEDAS
@@ -16,18 +41,13 @@ while(opcion != 1 and opcion != 2):
     try:
         opcion = int(input("Ingrese la opción que desee : "))
         if opcion == 1:
-            monto_origen = float(input('Ingrese monto en soles:'))
-            monto_destino = monto_origen / TIPO_CAMBIO
-            #SALIDA
-            print(f'El monto en dolares es {monto_destino}')
+            convertir_monto(opcion_1)
         elif opcion == 2:
-            monto_origen = float(input('Ingrese monto en dolares:'))
-            monto_destino = monto_origen * TIPO_CAMBIO
-            print(f'El monto en soles es {monto_destino}')
+            convertir_monto(opcion_2)
         else:
             print("LA OPCIÓN NO ES VALIDA : ingrese 1 o 2")
-    except:
-        print("Debe ingresar un número ")
+    except Exception as error:
+        print("Error : ",error)
     
 
         
