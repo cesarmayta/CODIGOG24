5ada913c8ce69a35f21b23b1fcfababd9bb28161
servilex <schabaneix@servilex.pe>
2024-01-26 20:54:53 -0500

    dia 5 paleta jugador

--- dia5/videojuego/main.py
+++ dia5/videojuego/main.py
@@ -9,6 +9,8 @@ FONDO = (0,0,64)
 pygame.init()
 #creamos reloj para mover la bolita mas lento
 reloj = pygame.time.Clock()
+#ajustar repetición de eventos para presion de tecla
+pygame.key.set_repeat(30)
 
 
 class Bolita(pygame.sprite.Sprite):
@@ -28,6 +30,29 @@ class Bolita(pygame.sprite.Sprite):
         elif self.rect.right > ANCHO or self.rect.left <= 0:
             self.speed[0] = -self.speed[0]
         self.rect.move_ip(self.speed)
+        
+class Paleta(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('imagenes/paleta.png')
+        self.rect = self.image.get_rect()
+        
+        #posición inicial de la paleta
+        self.rect.midbottom = (ANCHO /2,ALTO - 20)
+        #velocidad inicial
+        self.speed = [0,0]
+        
+    def update(self,evento):
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5,0]
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5,0]
+        else:
+            self.speed = [0,0]
+            
+        self.rect.move_ip(self.speed)
+        
     
 
 pantalla = pygame.display.set_mode((ANCHO,ALTO))
@@ -35,6 +60,7 @@ pygame.display.set_caption('Mi primer videojuego con python')
 
 
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
     #establecer el tiempo del reloj
@@ -42,11 +68,14 @@ while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
             
     #movemos la bolita
     bolita.update()
             
     pantalla.fill(FONDO)
     pantalla.blit(bolita.image,bolita.rect)
+    pantalla.blit(jugador.image,jugador.rect)
     #actualizar los objetos en la pantalla
     pygame.display.flip()
