ac08dab65de511c8e87b6f596b3c6fc6ed18e350
servilex <schabaneix@servilex.pe>
2024-01-29 22:57:30 -0500

    dia1 requesta con path

--- dia1/web/web.py
+++ dia1/web/web.py
@@ -11,4 +11,24 @@ def saludo():
     nombre = request.args.get('nombre','nn')
     return f'<center><b>hola {nombre}</b></center>'
 
+@app.route('/suma')
+def suma():
+    n1 = request.args.get('n1','0')
+    n2 = request.args.get('n2','0')
+    resultado = int(n1) + int(n2)
+    return f'<h1><center>LA SUMA DE {n1} + {n2} ES {resultado}</center></h1>'
+
+@app.route('/<operacion>/<int:n1>/<int:n2>')
+def calculadora(operacion='niguna',n1=0,n2=0):
+    if operacion == 'suma':
+        resultado = n1 + n2
+    elif operacion == 'resta':
+        resultado = n1 - n2
+    elif operacion == 'multiplicacion':
+        resultado = n1 * n2
+    else:
+        resultado = 'nn'
+
+    return f'<h1> la {operacion} de {n1} y {n2} es {resultado}</h1>'
+
 app.run(debug=True)
