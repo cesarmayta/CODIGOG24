19e50fe4a75335e6703cd89954c39e1c7bd482f3
servilex <schabaneix@servilex.pe>
2024-01-30 20:52:31 -0500

    plantillas

--- dia2/app.py
+++ dia2/app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    nombre_args = request.args.get('nombre','nn')
+    nombre_args = request.args.get('nombre','CESAR')
+    lista_monedas = ['soles','dolares','euros']
     #monto_origen = request.args.get('monto_origen','0')
     monto_destino = 0
     if request.method == 'POST':
         monto_origen = request.form['monto_origen']
         monto_destino = int(monto_origen) / 3.8
+        monto_destino = round(monto_destino,2)
         
-    return render_template('index.html',nombre=nombre_args,monto_destino=monto_destino)
+    return render_template('index.html',nombre=nombre_args,
+                           monto_destino=monto_destino,monedas=lista_monedas)
 
 app.run(debug=True)
