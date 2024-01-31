e7331da1abf9fdcbb8059d7475fa7eb789da150f
servilex <schabaneix@servilex.pe>
2024-01-30 22:37:24 -0500

    herencia de plantillas

--- dia2/contactos/app.py
+++ dia2/contactos/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
