d18fab72ed8391ba020c65bc453969ea41810c71
servilex <schabaneix@servilex.pe>
2024-02-05 20:29:07 -0500

    base de datos

--- dia1/run.py
+++ dia1/run.py
@@ -1,6 +1,16 @@
 from flask import Flask,request,jsonify
+from flask_mysqldb import MySQL 
+
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'api_rest'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
