db29a56bb0b82e40c315738f15dd600ee11f14c1
servilex <schabaneix@servilex.pe>
2024-02-06 22:23:26 -0500

    cors

--- dia2/frontend/src/App.jsx
+++ dia2/frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 class App extends React.Component{
+
+    componentDidMount(){
+      console.log('cargando tareas...');
+      axios.get('http://localhost:5000/tarea')
+      .then(res=>{
+        console.log(res.data.content)
+      }
+      )
+    }
 
     render(){
       return(
