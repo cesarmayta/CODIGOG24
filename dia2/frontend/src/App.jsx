4b54bf05a4690f6cadd1e227129caa4680b88429
servilex <schabaneix@servilex.pe>
2024-02-06 22:34:25 -0500

    mostrar tareas

--- dia2/frontend/src/App.jsx
+++ dia2/frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container,Table} from 'react-bootstrap'
 
 class App extends React.Component{
+
+    constructor(props){
+      super(props)
+      this.state = ({
+        tareas:[]
+      })
+    }
 
     componentDidMount(){
       console.log('cargando tareas...');
       axios.get('http://localhost:5000/tarea')
       .then(res=>{
         console.log(res.data.content)
+        this.setState({
+          tareas : res.data.content
+        })
       }
       )
     }
@@ -15,7 +27,30 @@ class App extends React.Component{
     render(){
       return(
         <div>
-          <h1>Lista de Tareas</h1>
+          <Container>
+            <h1>Lista de Tareas</h1>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Tarea</th>
+                  <th>Estado</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.tareas.map((tarea,index)=>{
+                  return(
+                    <tr key={index}>
+                      <td>{tarea.id}</td>
+                      <td>{tarea.descripcion}</td>
+                      <td>{tarea.estado}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
+          </Container>
+          
         </div>
       )
     }
