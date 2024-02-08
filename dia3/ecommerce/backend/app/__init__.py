a0009ab838ef3bd33b7632d0500eda334f5aa9c3
servilex <schabaneix@servilex.pe>
2024-02-07 21:24:23 -0500

    config db

--- dia3/ecommerce/backend/app/__init__.py
+++ dia3/ecommerce/backend/app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 from .shop import shop
 
+from .config import Config
+
 def create_app():
     
     app = Flask(__name__)
+    app.config.from_object(Config)
     app.register_blueprint(shop)
     
     return app
