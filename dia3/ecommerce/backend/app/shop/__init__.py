7dd44f86349fa5b670675af29ad7c6c84a13bfc2
servilex <schabaneix@servilex.pe>
2024-02-07 20:03:48 -0500

    primer endpoint desde blueprint

--- dia3/ecommerce/backend/app/shop/__init__.py
+++ dia3/ecommerce/backend/app/shop/__init__.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
 
 shop = Blueprint('shop',__name__,url_prefix='/')
+
+@shop.route('/')
+def index():
+    context = {
+        'message':'prueba de endpoint'
+    }
+    return jsonify(context)
+    
