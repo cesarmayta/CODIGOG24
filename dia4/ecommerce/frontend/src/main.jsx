5404fc23536a93238a06ce6714491e88ed56b19f
servilex <schabaneix@servilex.pe>
2024-02-08 22:51:14 -0500

    dia4 frontend get marcas

--- dia4/ecommerce/frontend/src/main.jsx
+++ dia4/ecommerce/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import router from './router/Router';
-import './index.css'
+//import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
