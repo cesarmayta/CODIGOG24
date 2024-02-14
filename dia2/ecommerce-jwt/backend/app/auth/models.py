e37fa8b7eac556dc250c11af041155595b10f36a
cesar mayta <cesarmayta@gmail.com>
2024-02-13 22:24:48 -0500

    dia2 usuario

--- dia2/ecommerce-jwt/backend/app/auth/models.py
+++ dia2/ecommerce-jwt/backend/app/auth/models.py
@@ -7,3 +7,24 @@ class Usuario(db.Model):
     email = db.Column(db.String(200),nullable=False,unique=True)
     password = db.Column(db.Text,nullable=False)
     is_admin = db.Column(db.Boolean,default=False)
+
+    @staticmethod
+    def get_all():
+        return Usuario.query.all()
+
+    @staticmethod
+    def get_by_id(id):
+        return Usuario.query.get(id)
+
+    @staticmethod
+    def get_by_email(email):
+        return Usuario.query.filter_by(email=email).first()
+
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
