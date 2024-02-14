cb2e4aa11112b074f1e1573968fb7665da091a46
cesar mayta <cesarmayta@gmail.com>
2024-02-13 22:34:51 -0500

    usuario post

--- dia2/ecommerce-jwt/backend/app/auth/resources/usuario.py
+++ dia2/ecommerce-jwt/backend/app/auth/resources/usuario.py
@@ -3,9 +3,32 @@ from flask import request
 from .. import auth
 from ..models import Usuario
 
+from werkzeug.security import(
+    generate_password_hash,
+    check_password_hash
+)
+
 api = Api(auth)
 
 class UsuarioResource(Resource):
+
+    def post(self):
+        data = request.get_json()
+        password = data['password']
+        password_hash = generate_password_hash(password)
+        usuario = Usuario()
+        usuario.email = data['email']
+        usuario.password = password_hash
+        usuario.save()
+
+        context = {
+            'status':True,
+            'message':'usuario registrado'
+        }
+
+        return context
+
+        
 
     def get(self):
         context = {
