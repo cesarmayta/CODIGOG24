72b9d4745a3609203708c85fa52c8f74612eb999
cesar mayta <cesarmayta@gmail.com>
2024-02-15 20:22:38 -0500

    jwt login

--- dia2/ecommerce-jwt/backend/app/auth/resources/usuario.py
+++ dia2/ecommerce-jwt/backend/app/auth/resources/usuario.py
@@ -8,6 +8,11 @@ from werkzeug.security import(
     check_password_hash
 )
 
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required
+)
+
 api = Api(auth)
 
 class UsuarioResource(Resource):
@@ -37,4 +42,44 @@ class UsuarioResource(Resource):
         }
         return context
 
+class AuthenticationResource(Resource):
+
+    def post(self):
+        try:
+            data = request.get_json()
+            email = data['email']
+            password = data['password']
+            usuario = Usuario.get_by_email(email)
+            if not usuario:
+                context = {
+                    'status':False,
+                    'message':'no se encontro usuario con ese email'
+                }
+                return context,404
+
+            is_password_checked = check_password_hash(usuario.password,
+                                                      password)
+            
+            if is_password_checked:
+               token = create_access_token(identity=usuario.id)
+               context = {
+                    'status':True,
+                    'content':token
+               }
+               return context,201
+            else:
+                context = {
+                    'status':False,
+                    'message': 'contraseña incorrecta'
+                }
+                return context,400
+        except Exception as e:
+            context = {
+                'status':False,
+                'message':str(e)
+            }
+
+            return context,500
+
 api.add_resource(UsuarioResource,'/user')
+api.add_resource(AuthenticationResource,'/login')
