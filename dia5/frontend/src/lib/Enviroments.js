57f5ab1394003336cb9b7d79560b3f00760123f6
cesar mayta <cesarmayta@gmail.com>
2024-02-19 22:42:08 -0500

    frontend

---
+++ dia5/frontend/src/lib/Enviroments.js
@@ -0,0 +1,3 @@
+// export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "https://flask-shop-api-g24.onrender.com";
+// export const API_URL = "https://immense-shelf-43405.herokuapp.com";
