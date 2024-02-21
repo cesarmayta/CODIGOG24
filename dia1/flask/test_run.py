b9847c08e4be6ff27b3badec22d1c56ff88728d9
cesar mayta <cesarmayta@gmail.com>
2024-02-20 21:33:16 -0500

    dia1 pytest con flask

---
+++ dia1/flask/test_run.py
@@ -0,0 +1,12 @@
+import pytest
+from run import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'prueba flask' in response.data
