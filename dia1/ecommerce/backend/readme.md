fe38987ad0a5c4911300cdaae8c6884de9d8fcf0
cesar mayta <cesarmayta@gmail.com>
2024-02-20 19:40:51 -0500

    dia1

--- dia1/ecommerce/backend/readme.md
+++
@@ -1,5 +0,0 @@
-#para hacer migraciones
-flask db init(solo se ejecuta una vez)
-
-flask db migrate -m "categorias"
-flask db upgrade
--- dia3/backend-jwt/app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI='mysql://root:@localhost/db_shop_flask'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    JWT_SECRET_KEY='QWERTY123'
