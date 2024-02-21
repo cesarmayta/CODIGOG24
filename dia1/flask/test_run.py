78c350e9daf906e5b8e45a249a6606f676292f2a
cesar mayta <cesarmayta@gmail.com>
2024-02-20 22:15:19 -0500

    test post tarea

--- dia1/flask/test_run.py
+++ dia1/flask/test_run.py
@@ -19,3 +19,13 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'prueba flask' in response.data
+
+class TestTareaEndpoints:
+    def test_set_tarea(self,client):
+        tarea_data = {
+            'descripcion': 'tarea 1',
+            'estado':'pendiente'
+        }
+        response = client.post('/tarea',json=tarea_data)
+        assert response.status_code == 200
+        assert b'tarea 1' in response.data
