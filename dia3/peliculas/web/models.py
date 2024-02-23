bb84b3706de33e508fe1763965bef82aa9183739
cesar mayta <cesarmayta@gmail.com>
2024-02-22 22:23:17 -0500

    views para peliculas

--- dia3/peliculas/web/models.py
+++ dia3/peliculas/web/models.py
@@ -5,3 +5,6 @@ class Pelicula(models.Model):
     titulo = models.CharField(max_length=200)
     imagen = models.CharField(max_length=255)
     puntaje = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
