d14b3c69f6b6c4a5430f5b1bf722308b2414b4de
servilex <schabaneix@servilex.pe>
2024-02-24 11:18:45 -0500

    registrar nueva tarea

--- dia5/repaso/todolist/web/views.py
+++ dia5/repaso/todolist/web/views.py
@@ -4,6 +4,10 @@ from .models import Tarea
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        descripcion = request.POST.get('descripcion')
+        Tarea.objects.create(descripcion=descripcion)
+    
     lista_tareas = Tarea.objects.all()
     print(lista_tareas)
     context = {
