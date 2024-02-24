3918f59aa6938235b31bcb6c690c202ba6a72321
servilex <schabaneix@servilex.pe>
2024-02-24 10:12:53 -0500

    dia5 modelos

--- dia5/repaso/todolist/web/models.py
+++ dia5/repaso/todolist/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    descripcion = models.CharField(max_length=200)
+    fecha_creacion = models.DateField(auto_now=True)
+    estado = models.CharField(max_length=20)
