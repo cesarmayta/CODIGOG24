9d8e6c11b5aa16f28790b11512c19b8ca6598270
César Mayta <cesarmayta@gmail.com>
2024-02-27 21:42:46 -0500

    view index con modelos

--- dia2/ecommerce/web/views.py
+++ dia2/ecommerce/web/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .models import (
+    Categoria,Marca,Producto,
+    ProductoImagen
+)
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
+    lista_productos = Producto.objects.all()
+    
+    context = {
+        'categorias':lista_categorias,
+        'marcas':lista_marcas,
+        'productos':lista_productos
+    }
+    return render(request,'index.html',context)
