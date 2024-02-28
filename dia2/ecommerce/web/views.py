cbf1b6790df0d9048e5a8281edbb979a09f34a71
César Mayta <cesarmayta@gmail.com>
2024-02-27 22:23:47 -0500

    filtro por marca

--- dia2/ecommerce/web/views.py
+++ dia2/ecommerce/web/views.py
@@ -11,9 +11,13 @@ def index(request):
     lista_marcas = Marca.objects.all()
     
     categoria_id = request.GET.get('cat')
+    marca_id = request.GET.get('mar')
     if categoria_id:
         categoria_seleccionada = Categoria.objects.get(pk=categoria_id)
         lista_productos = Producto.objects.filter(categoria=categoria_seleccionada)
+    elif marca_id:
+        marca_seleccionada = Marca.objects.get(pk=marca_id)
+        lista_productos = Producto.objects.filter(marca=marca_seleccionada)
     else:
         lista_productos = Producto.objects.all()
     
