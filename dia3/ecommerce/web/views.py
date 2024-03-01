e4edce12be789d03bb67db74e884d8ed39bc168e
César Mayta <cesarmayta@gmail.com>
2024-02-29 19:51:28 -0500

    dia3 carrito

--- dia3/ecommerce/web/views.py
+++ dia3/ecommerce/web/views.py
@@ -5,7 +5,7 @@ from .models import (
     ProductoImagen
 )
 
-# Create your views here.
+############### CATALOGO DE PRODUCTOS #####################
 def index(request):
     lista_categorias = Categoria.objects.all()
     lista_marcas = Marca.objects.all()
@@ -34,3 +34,7 @@ def producto(request,producto_id):
         'producto':obj_producto
     }
     return render(request,'producto.html',context)
+
+########### CARRITO DE COMPRAS ############################
+def carrito(request):
+    return render(request,'carrito.html')
