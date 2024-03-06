a53d11cc9c751ec0cb1ecd3473d98320c66641a7
César Mayta <cesarmayta@gmail.com>
2024-03-05 20:14:55 -0500

    get peliculas

--- dia1/peliculas/api/views.py
+++ dia1/peliculas/api/views.py
@@ -1,9 +1,20 @@
 from django.http import JsonResponse
+from .models import Pelicula
 
 def index(request):
     context = {
         'status':True,
         'message':'mi primer api rest con django'
+    }
+    
+    return JsonResponse(context)
+
+def pelicula(request):
+    lista_peliculas = Pelicula.objects.all()
+    
+    context = {
+        'status':True,
+        'content':list(lista_peliculas.values())
     }
     
     return JsonResponse(context)
