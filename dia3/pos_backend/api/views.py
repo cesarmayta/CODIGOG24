a11fb776215b6dd99a5f208cbd72cd94c6de888c
César Mayta <cesarmayta@gmail.com>
2024-03-06 21:48:47 -0500

    vistas genericas

--- dia3/pos_backend/api/views.py
+++ dia3/pos_backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import (
+    Mesa,Categoria
+)
+
+from .serializers import (
+    CategoriaSerializer,
+    MesaSerializer
+)
+
+class CategoriaView(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+class MesaView(generics.ListCreateAPIView):
+    queryset = Mesa.objects.all()
+    serializer_class = MesaSerializer
+    
+    
