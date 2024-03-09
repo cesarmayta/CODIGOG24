faffa17fd20c9db2f5502dc57137686330af8e80
César Mayta <cesarmayta@gmail.com>
2024-03-08 19:59:39 -0500

    session auth

--- dia5/autenticacion/api/views.py
+++ dia5/autenticacion/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from rest_framework.authentication import (
-    BasicAuthentication
+    BasicAuthentication,SessionAuthentication
 )
 
 from rest_framework.permissions import (
@@ -19,7 +19,7 @@ class IndexView(APIView):
         return Response(context)
     
 class UsuarioView(APIView):
-    authentacion_classes = [BasicAuthentication]
+    authentacion_classes = [BasicAuthentication,SessionAuthentication]
     permission_classes = [IsAuthenticated]
     
     def get(self,request):
