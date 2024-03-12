a49a84483d18735c257e83b1706c9d39c5cae554
César Mayta <cesarmayta@gmail.com>
2024-03-11 22:58:44 -0500

    dia1 os

--- dia1/05-os.js
+++ dia1/05-os.js
@@ -11,3 +11,22 @@ console.log('modelo procesador : ' + cpu[0].model);
 
 const memoria = os.totalmem();
 console.log('memoria ram : '+ memoria);
+
+/* implementar con promesas un función que retorne la memoria
+en kb,mb y gb
+1024 bytes = 1kb
+1024 kb = 1mb
+1024 mb = 1gb
+*/
+function calcularMemoria(capacidad,tipo){
+    return new Promise((res,rej)=>{
+        let memoriaConvertida = capacidad / 1024
+        console.log(`MEMORIA EN ${tipo} : ${memoriaConvertida.toFixed(2)}`);
+        res(memoriaConvertida);
+        rej('error...')
+    })
+}
+
+calcularMemoria(memoria,'KB')
+    .then((kb)=>calcularMemoria(kb,'MB'))
+    .then((mb)=>calcularMemoria(mb,'GB'))
