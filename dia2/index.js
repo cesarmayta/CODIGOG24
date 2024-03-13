486d197c05fe1445b6e746d8a1157ab3a7643b0b
César Mayta <cesarmayta@gmail.com>
2024-03-12 19:56:14 -0500

    request

--- dia2/index.js
+++ dia2/index.js
@@ -6,4 +6,16 @@ app.get('/',(req,res)=>{
     res.send('<h1><center>Mi primer servidor con express.js y Nodejs</center></h1>')
 })
 
+app.get('/api',(req,res)=>{
+    res.json({
+        'status':true,
+        'message':'api rest activo con express'
+    })
+})
+
+app.get('/saludo',(req,res)=>{
+    let nombre = req.query.nombre;
+    res.send(`<h1><center><i>Hola ${nombre}</i></center></h1>`);
+})
+
 app.listen(5000,()=>console.log('servidor en http://127.0.0.1:5000'))
