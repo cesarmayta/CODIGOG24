5ff7d0e7f882ff7f3f8c26c4f15016b557e506bf
César Mayta <cesarmayta@gmail.com>
2024-03-12 21:23:38 -0500

    database

---
+++ dia2/apirest/database.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2')
+
+const mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'db_tareas'
+});
+
+mysqlConnection.connect(function(err){
+    if(err){
+        console.error(err);
+        return;
+    }else{
+        console.log('conectado a la bd');
+    }
+})
+
+module.exports = mysqlConnection
+
+/*mysqlConnection.query('select * from tarea',(err,rows,fields)=>{
+    console.log(rows);
+})*/
