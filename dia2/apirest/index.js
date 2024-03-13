d265298f60db17394c01bd893ae5e1f7a9d2f6f7
César Mayta <cesarmayta@gmail.com>
2024-03-12 22:46:25 -0500

    cors y frontend

--- dia2/apirest/index.js
+++ dia2/apirest/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const mysqlConnection = require('./database');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=>{
