8518d2689b365aafd8bfd7d7c1c832117d6d9c28
César Mayta <cesarmayta@gmail.com>
2024-03-12 20:04:03 -0500

    req.params

--- dia2/index.js
+++ dia2/index.js
@@ -18,4 +18,10 @@ app.get('/saludo',(req,res)=>{
     res.send(`<h1><center><i>Hola ${nombre}</i></center></h1>`);
 })
 
+app.get('/suma/:n1/:n2',(req,res)=>{
+    const {n1,n2} = req.params;
+    let suma = parseInt(n1) + parseInt(n2)
+    res.send(`la suma de ${n1} + ${n2} es ${suma}`);
+})
+
 app.listen(5000,()=>console.log('servidor en http://127.0.0.1:5000'))
