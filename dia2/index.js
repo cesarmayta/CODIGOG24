1d66a718bb35e90ad1b6a497d54bbebfe7213dd1
César Mayta <cesarmayta@gmail.com>
2024-03-12 19:43:43 -0500

    dia2 nodemon

--- dia2/index.js
+++ dia2/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Mi primer servidor con express.js</h1>')
+    res.send('<h1><center>Mi primer servidor con express.js y Nodejs</center></h1>')
 })
 
 app.listen(5000,()=>console.log('servidor en http://127.0.0.1:5000'))
