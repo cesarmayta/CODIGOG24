076f5cdcb55932eb6097b04a47a67f76e4c44bca
César Mayta <cesarmayta@gmail.com>
2024-03-13 21:53:00 -0500

    dia4

---
+++ dia4/ecommerce_backend/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const {config} = require('./config')
+
+const categoryApi = require('./routes/category.routes')
+
+const app = express()
+/******** MIDDLEWARES **********/
+app.use(express.json())
+app.use(function(req,res,next){
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed)
+    console.log('ejecutado a las ',today.toISOString())
+    next()
+})
+
+app.use('/usuario',(req,res,next)=>{
+    console.log('tipo de request',req.method)
+    next()
+})
+
+app.get('/',(req,res)=>{
+    res.json({
+        'status':true,
+        'message':'api rest ecommerce version 1.0'
+    })
+})
+
+app.get('/usuario',(req,res)=>{
+    res.json({
+        nombre:'cesar'
+    })
+})
+
+categoryApi(app)
+
+app.listen(config.port,
+    ()=>console.log(`http://127.0.0.1:${config.port}`))
---
+++ dia4/ecommerce_backend/src/services/category.service.js
@@ -0,0 +1,48 @@
+const MysqlLib = require('../lib/mysql')
+
+class CategoryService{
+
+    constructor(){
+        this.db = new MysqlLib()
+    }
+
+    async getAll(){
+        const query = 'select id,name from tbl_category'
+        const result = await this.db.querySql(query)
+        return result
+    }
+
+    async create({data}){
+        const query = `insert into tbl_category(name)
+                       values ('${data.name}')`
+        await this.db.querySql(query)
+        const queryLast = 'select id,name from tbl_category order by id desc limit 1'
+        const result = await this.db.querySql(queryLast)
+        return result
+    }
+
+    async getById(id){
+        const query = `select id,name from tbl_category where id= '${id}'`
+        const result = await this.db.querySql(query)
+        return result
+    }
+
+    async update({data,id}){
+        const query = `update tbl_category set
+                       name = '${data.name}'
+                       where id= ${id}`
+        await this.db.querySql(query)
+        const queryUpdated = `select id,name from tbl_category where id = ${id}`
+        const result = await this.db.querySql(queryUpdated)
+        return result
+    }
+
+    async delete(id){
+        const query = `delete from tbl_category 
+                       where id=${id}`
+        await this.db.querySql(query)
+        return true
+    }
+}
+
+module.exports = CategoryService
