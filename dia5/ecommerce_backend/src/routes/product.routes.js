0fd18b69444d3fe04d4c802021ac82b059349914
César Mayta <cesarmayta@gmail.com>
2024-03-15 22:34:48 -0500

    product delete

--- dia5/ecommerce_backend/src/routes/product.routes.js
+++ dia5/ecommerce_backend/src/routes/product.routes.js
@@ -50,6 +50,18 @@ function productApi(app){
         }
     })
 
+    router.delete('/:id',async function(req,res){
+        try{
+            const {id} = req.params
+            const data = await objProduct.delete(id)
+            if(data){
+                res.sendStatus(201)
+            }
+        }catch(err){
+            res.status(500).json(boom.badData(err))
+        }
+    })
+
 }
 
 module.exports = productApi
