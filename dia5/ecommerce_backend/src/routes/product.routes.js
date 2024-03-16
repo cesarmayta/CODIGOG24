27c8b9f8583e9ed534e5b6f008943376e7d25c43
César Mayta <cesarmayta@gmail.com>
2024-03-15 22:16:06 -0500

    get by id product

--- dia5/ecommerce_backend/src/routes/product.routes.js
+++ dia5/ecommerce_backend/src/routes/product.routes.js
@@ -28,6 +28,17 @@ function productApi(app){
             res.status(500).json(boom.badData(err))
         }
     })
+
+    router.get('/:id',async function(req,res){
+        try{
+            const {id} = req.params
+            const data = await objProduct.findOne(id)
+            res.status(200).json(data)
+        }catch(err){
+            res.status(500).json(boom.badData(err))
+        }
+    })
+
 }
 
 module.exports = productApi
