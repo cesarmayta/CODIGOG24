472daa1c37c7e71291410ff5f5153c9065caf7e0
César Mayta <cesarmayta@gmail.com>
2024-03-19 22:00:08 -0500

    mongoose

--- dia1/mern_ecommerce/backend/src/config/index.js
+++ dia1/mern_ecommerce/backend/src/config/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const config = {
-    port : process.env.PORT || 5000
+    port : process.env.PORT || 5000,
+    mongoUri: process.env.MONGOURI || 'mongodb://localhost:27017/db_ecommerce_g24'
 }
 
 module.exports = {config}
