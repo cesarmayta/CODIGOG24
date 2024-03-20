352604c2a3e7e5e5f212fbd295ade924c65f622d
César Mayta <cesarmayta@gmail.com>
2024-03-19 20:08:51 -0500

    dia1

--- dia2/apirest/readme.md
+++
@@ -1,9 +0,0 @@
-# paso 1
-npm init -y
-
-# paso 2
-npm install express mysql2
-
-# paso 3
-npm install --save-dev nodemon
-
--- dia6/ecommerce-prisma/src/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const config = {
-    port:process.env.PORT || 5000
-}
-
-module.exports = {config}
