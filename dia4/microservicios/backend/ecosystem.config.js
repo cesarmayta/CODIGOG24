99c1071ee22b2cbb60ef4253a3effecedde5ce23
César Mayta <cesarmayta@gmail.com>
2024-03-22 21:54:14 -0500

    pm2

---
+++ dia4/microservicios/backend/ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "ms-catalog",
+    script : "./src/microservices/ms-catalog/index.js"
+  },{
+    name   : "ms-users",
+    script : "./src/microservices/ms-users/index.js"
+  }]
+}
